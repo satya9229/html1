83dc9dde9e2d015e38f03536c488fc921fef08b6
satya1712 <satya@650sasi.ac.in>
2023-02-04 06:13:46 +0530

    my commit

---
+++ myscripts.js
@@ -0,0 +1,10 @@
+function calculateSum() {
+    var num1 = document.getElementById("num1").value
+    var num2 = document.getElementById("num2").value
+    var sum = parseInt(num1) + parseInt(num2)
+    console.log(sum)
+    // document.write(sum)
+    // document.getElementById("result").innerHTML=sum;
+    alert(sum)
+
+  }
